76715ea01d6c69e58b946a7189de067e65b40ea5
Mike Rotondo <mrotondo@google.com>
2017-05-12 07:32:55 -0700

    Cleanup

--- carousel/carousel.js
+++ carousel/carousel.js
@@ -1,25 +1,14 @@
 function CreateMonument() {
     var testMonument = new Monument(
+        // LAT/LON COORDINATES
         37.752409, -122.418262,
+        // ALTITUDE
         0,
+        // HEADING
         Math.PI / 2,
+        // INIT FUNCTION
         function (group) {
-            this.positionClones = function (angleOffset) {
-                var ringRadius = 5;
-                for (var i = 0; i < this.clones.length; i++) {
-                    var baseAngle = (i / this.clones.length) * 2 * Math.PI;
-                    var angle = baseAngle + angleOffset;
-                    var clone = this.clones[i];
-                    clone.position.set(
-                        ringRadius * Math.cos(angle),
-                        0,
-                        ringRadius * Math.sin(angle));
-                    clone.rotation.set(0, -angle, 0);
-                }
-            }
-
-            this.fakeTime = 0;
-
+            // EXAMPLE OF OBJ/MTL LOADING, CLONING & STORING IN AN ARRAY
             var onProgress = function (xhr) {
                 if (xhr.lengthComputable) {
                     var percentComplete = xhr.loaded / xhr.total * 100;
@@ -45,51 +34,28 @@ function CreateMonument() {
                         this.clones.push(clone);
                         group.add(clone);
                     }
-                    this.positionClones(this.angleOffset);
                 }.bind(this), onProgress, onError);
             }.bind(this));
+            // END EXAMPLE
 
-            var archMtlLoader = new THREE.MTLLoader();
-            archMtlLoader.setPath('/augmented-communities/models/arch-obj/');
-            archMtlLoader.load('obj.mtl', function (materials) {
-                materials.preload();
-                var archObjLoader = new THREE.OBJLoader();
-                archObjLoader.setMaterials(materials);
-                archObjLoader.setPath('/augmented-communities/models/arch-obj/');
-                archObjLoader.load('tinker.obj', function (object) {
-                    object.scale.set(0.2, 0.2, 0.2);
-                    object.rotation.set(-Math.PI / 2, 0, 0);
-                    group.add(object);
-
-                    var clone = object.clone();
-                    clone.rotateZ(Math.PI / 2);
-                    group.add(clone);
-
-                    var clone2 = object.clone();
-                    clone2.rotateZ(Math.PI / 4);
-                    group.add(clone2);
-
-                    var clone3 = object.clone();
-                    clone3.rotateZ(-Math.PI / 4);
-                    group.add(clone3);
-                }.bind(this), onProgress, onError);
-            }.bind(this));
-
+            // EXAMPLE OF DAE LOADING
             var trainLoader = new THREE.ColladaLoader();
             trainLoader.options.convertUpAxis = true;
             trainLoader.load( '/augmented-communities/models/train.dae', function ( collada ) {
                 var object = collada.scene;
-                object.scale.set(0.5, 0.5, 0.5);
+                object.scale.set(0.3, 0.3, 0.3);
                 object.rotateZ(Math.PI / 2);
                 this.centralObject = object;
                 group.add( object );
-            }.bind(this) );
+            }.bind(this));
+            // END EXAMPLE
 
+            // EXAMPLE OF THREE.JS EDITOR SCENE LOADING
             var ringsSceneLoader = new THREE.ObjectLoader();
             ringsSceneLoader.load(
                 '/augmented-communities/models/rings-scene.json',
                 function (obj) {
-                    obj.position.set(0, 12, 0);
+                    obj.position.set(0, 1.5, 0);
                     group.add( obj );
                 },
                 function ( xhr ) {
@@ -99,12 +65,27 @@ function CreateMonument() {
                     console.error( 'An error happened' );
                 }
             );
+            // END EXAMPLE
 
         },
+        // UPDATE FUNCTION
         function () {
+            if (this.fakeTime === undefined) {
+                this.fakeTime = 0;
+            }
             this.fakeTime += 0.01;
             if (this.clones !== undefined) {
-                this.positionClones(this.fakeTime)
+                var ringRadius = 5;
+                for (var i = 0; i < this.clones.length; i++) {
+                    var baseAngle = (i / this.clones.length) * 2 * Math.PI;
+                    var angle = baseAngle + this.fakeTime;
+                    var clone = this.clones[i];
+                    clone.position.set(
+                        ringRadius * Math.cos(angle),
+                        0,
+                        ringRadius * Math.sin(angle));
+                    clone.rotation.set(0, -angle, 0);
+                }
             }
             if (this.centralObject !== undefined)
             {
